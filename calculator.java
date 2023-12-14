5eba72a286f3335b2d4b40d96faaecdea46426e2
[sophiann32] <[sophiann32@gmail.com]>
2023-12-14 15:38:54 +0900

    merge

--- calculator.java
+++ calculator.java
@@ -1,6 +1,11 @@
-int my_add(int number1, int number2){
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+    int my_add(int number1, int number2){
 			if (number1 < 0 or number2 < 0)
 				return;
 			return number1+number2;
 }
 
+}
